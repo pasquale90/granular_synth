9d7ad4c29d12cfbb601ecd7ddedaf7ae18bff990
Paschalis Melissas <melissaspaschalis@gmail.com>
2025-09-30 18:36:50 +0300

    test deployment with reduced bmp file sizes

---
+++ data/BMP/emptybmp.py
@@ -0,0 +1,9 @@
+from PIL import Image
+
+width, height = 3840, 2160
+img = Image.new("RGB", (width, height), (0, 0, 0))
+# img.save("NEWGRANADE_UI_POSITIONSREFERENCE.bmp", "BMP")
+# img.save("GRANADE_UI_POSITIONSREFERENCE.bmp", "BMP")
+img.save("TREE.bmp", "BMP")
+
+
